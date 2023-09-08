35d2aa9f18bb64020d578dffd50b9e7fe7997ecc
Akinladejo <akinladejo.andrew@yahoo.com>
2023-09-08 16:23:33 -0700

    task 6

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int number = 0;
+
+	while (number <= 9)
+	{
+		number++;
+		printf("%d", number);
+	}
+	return (0);
+}
