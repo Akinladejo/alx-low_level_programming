418b5538781eed37b72188e2718c44911e10d7c0
Akinladejo <akinladejo.andrew@yahoo.com>
2023-09-08 17:55:33 -0700

    task-100

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -17,14 +17,13 @@ int main(void)
 		{
 			if (num1 != num2)
 			{
-				putchar(',');
-				putchar(' ');
+				putchar(num1 + '0');
+				putchar(num2 + '0');
 				if (num1  != 8 || num2 != 9)
 				{
 					putchar(',');
 					putchar(' ');
 				}
-				
 			}
 		}
 	}
