1e0f2054d3e0774cb63e2372ed899d8bd0e4c63f
Akinladejo <akinladejo.andrew@yahoo.com>
2023-09-08 20:06:15 -0700

    task 102

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -9,19 +9,25 @@
 int main(void)
 {
 	int number1;
+
 	int number2;
 
 	for (number1 = 0; number1 <= 99; number1++)
 	{
-		for (number2 = number1; number2 <= 99; number2++)
+		for (number2 = 0; number2 <= 99; number2++)
 		{
-			printf("%02d", number1);
-			printf(' ');
-			printf("%02d", number2);
-			if (number1 != 98 || number2 != 99)
+			if (number1 != number2)
 			{
-				putchar(',');
+				putchar(number1 / 10 + '0');
+				putchar(number1 % 10 + '0');
 				putchar(' ');
+				putchar(number2 / 10 + '0');
+				putchar(number2 % 10 + '0');
+				if (number2 < 99)
+				{
+					putchar(',');
+					putchar(' ');
+				}
 			}
 		}
 	}
