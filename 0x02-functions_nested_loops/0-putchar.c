e398b127c892d10010a88cf283b5165505491bfb
Akinladejo <akinladejo.andrew@yahoo.com>
2023-09-12 01:41:34 -0700

    task 1 commit- 0-putchar.c

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	char c = '_';
+
+	putchar(c);
+	putchar('\n');
+	return (0);
+}
