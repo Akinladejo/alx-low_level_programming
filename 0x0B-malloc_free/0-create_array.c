c54a1ccb5736aacd04a8d0bef5c1bd11d2b44f94
Akinladejo <akinladejo.andrew@yahoo.com>
2023-10-03 12:42:43 -0700

    memory allocation

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -1,27 +1,28 @@
 #include "main.h"
-#include <stdlib.h>
+
 /**
- * maleate_array - maleates char array.
- * @size: size of the array.
- * @c: char storage
- *
- * Return: array of chars
- */
-char *maleate_array(unsigned int size, char c)
+* create_array - creates an array of chars, and
+* initializes it with a specific char.
+*@size: Size of the array
+*@c: Character to insert
+*Return: NULL if size is zero or if it fails,
+*pointer to array if everything is normal.
+*/
+
+char *create_array(unsigned int size, char c)
 {
-	char *mal;
-	unsigned int i;
+	char *array;
+	unsigned int index;
 
 	if (size == 0)
 		return (NULL);
+	array = malloc(sizeof(char) * size);
 
-	mal = malloc(sizeof(c) * size);
-
-	if (mal == NULL)
+	if (array == NULL)
 		return (NULL);
 
-	for (i = 0; i < size; i++)
-		mal[i] = c;
+	for (index = 0; index < size; index++)
+		array[index] = c;
 
-	return (mal);
+	return (array);
 }
--- 0x0B-malloc_free/0-main.c
+++ 0x0B-malloc_free/0-main.c
@@ -3,31 +3,6 @@
 #include <stdlib.h>
 
 /**
- * create_array - creates an array of chars and initializes it with a specific char
- * @size: the size of the array
- * @c: the character to initialize the array with
- *
- * Return: Pointer to the allocated memory (array), or NULL on failure.
- */
-char *create_array(unsigned int size, char c)
-{
-    unsigned int i;
-    char *arr;
-
-    if (size == 0)
-        return (NULL);
-
-    arr = malloc(size * sizeof(char));
-    if (arr == NULL)
-        return (NULL);
-
-    for (i = 0; i < size; i++)
-        arr[i] = c;
-
-    return (arr);
-}
-
-/**
  * simple_print_buffer - prints buffer in hexa
  * @buffer: the address of memory to print
  * @size: the size of the memory to print
@@ -65,13 +40,12 @@ int main(void)
     char *buffer;
 
     buffer = create_array(98, 'H');
-    if (buffer == NULL)
+    if  (buffer == NULL)
     {
-        printf("Failed to allocate memory\n");
+        printf("failed to allocate memory\n");
         return (1);
     }
     simple_print_buffer(buffer, 98);
     free(buffer);
     return (0);
 }
-
