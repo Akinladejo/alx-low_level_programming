cedc8eae3116673ee0d350a5036f3dfb01992564
Akinladejo <akinladejo.andrew@yahoo.com>
2023-10-24 10:02:28 -0700

    103-find_loop.c

---
+++ 0x13-more_singly_linked_lists/103-find_loop.c
@@ -0,0 +1,41 @@
+#include "lists.h"
+#include <stdio.h>
+
+/**
+ * find_listint_loop - loop through  listint_t
+ * @head: points to listint_t
+ *
+ * Return: 0 if valid else Err
+ */
+
+listint_t *find_listint_loop(listint_t *head)
+{
+	listint_t *first, *second;
+
+	if (head->next == 0)
+		return (0);
+
+	first = head->next;
+	second = (head->next)->next;
+
+	while (second)
+	{
+		if (first == second)
+		{
+			first = head;
+
+			while (first != second)
+			{
+				first = first->next;
+				second = second->next;
+			}
+
+			return (first);
+		}
+
+		first = first->next;
+		second = (second->next)->next;
+	}
+
+	return (0);
+}
