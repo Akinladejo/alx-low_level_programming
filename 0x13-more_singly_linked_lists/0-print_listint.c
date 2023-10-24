e5497878033a554110180ab34a4963cbaba58702
Akinladejo <akinladejo.andrew@yahoo.com>
2023-10-24 01:00:38 -0700

    0-print_listint.c

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -12,15 +12,11 @@ size_t print_listint(const listint_t *h)
 {
 	size_t count = 0;
 
-	if (h)
+	while (h)
 	{
 		count++;
 		printf("%d\n", h->n);
 		h = h->next;
-	}
-	else
-	{
-		printf("error in code");
 	}
 
 	return (count);
