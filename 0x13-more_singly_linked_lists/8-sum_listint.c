ac5216ae96aa549ba9b3d5fec41babea67de7880
Akinladejo <akinladejo.andrew@yahoo.com>
2023-10-24 01:52:23 -0700

    8-sum_listint.c

---
+++ 0x13-more_singly_linked_lists/8-sum_listint.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+#include <stdio.h>
+
+/**
+ * sum_listint - sum of listint_t
+ * @head: points to listint_t list
+ *
+ * Return: 0 if valid else NULL
+ */
+int sum_listint(listint_t *head)
+{
+	int sum = 0;
+
+	if (head)
+	{
+		sum += head->n;
+		head = head->next;
+	}
+	else
+		printf("error");
+	return (sum);
+}
